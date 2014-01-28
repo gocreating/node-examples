7697fb9453e0eff0df9cac3f2bd9b3a0c504aae0
gocreating <gocreating@gmail.com>
2014-01-29 00:10:21 +0800

    Complete user-system functionality

--- user-system/app.js
+++ user-system/app.js
@@ -1,37 +1,64 @@
 
-/**
+/*
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express		= require('express'),
+	http 		= require('http'),
+	path		= require('path'),
+	routesBasic = require('./routes/basic'),
+	routesUser  = require('./routes/user');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('your secret here'));
+	app.use(express.session());
+	
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	// Authorization
+	app.use(function (req, res, next) {
+		if (req.session.isAuth == undefined) {
+			req.session.isAuth = false;
+		}
+		next();
+	});
+
+	// Messages
+	app.use(function (req, res, next) {
+		var err  = req.session.err,
+			succ = req.session.succ;
+		delete req.session.err;
+		delete req.session.succ;
+		res.locals.msg = '';
+		if (err) {
+			res.locals.msg = err
+		};
+		if (succ) {
+			res.locals.msg = succ;
+		}
+	    next();
+	});
+
+	app.use(app.router);
+});
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routesBasic(app);
+routesUser(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- user-system/routes/user.js
+++ user-system/routes/user.js
@@ -1,8 +1,148 @@
+var User = require('../models/user');
 
-/*
- * GET users listing.
- */
+// To prevent blank data or injection
+function isDataError(req, email, password) {
+	if (email == '') {
+		req.session.err = 'Empty email';
+		return true;
+	}
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+	if (password == '') {
+		req.session.err = 'Empty password.';
+		return true;
+	}
+
+	if (email.length > 35) {
+		req.session.err = 'Email can\'t be more than 35 characters.';
+		return true;
+	}
+
+	if (password.length > 35) {
+		req.session.err = 'Password can\'t be more than 35 characters.';
+		return true;
+	}
+
+	return false;
+}
+
+// Routing
+module.exports = function(app) {
+	// Prevent an authorized user from logging in again
+	app.get('/user/login', function(req, res, next) {
+		if (req.session.isAuth) {
+			res.render('err/authorized', {
+				title: 'Authorized'
+			});
+		} else {
+			next();
+		}
+	});
+
+	// Prevent an unauthorized user from logging out
+	app.get('/user/logout', function(req, res, next) {
+		if (req.session.isAuth) {
+			next();
+		} else {
+			res.render('err/unauthorized', {
+				title: 'Unauthorized'
+			});
+		}
+	});
+
+	// Authorized page
+	app.get('/user/auth/*', function(req, res, next) {
+		if (req.session.isAuth) {
+			next();
+		} else {
+			res.render('err/unauthorized', {
+				title: 'Unauthorized'
+			});
+		}
+	});
+
+	// Register
+	app.get('/user/register', function(req, res) {
+		res.render('user/register', {
+			title: 'Register'
+		});
+	});
+
+	app.post('/user/register', function(req, res) {
+		var email    = req.body.email;
+		var password = req.body.password;
+
+		if (isDataError(req, email, password)) {
+			res.redirect('/user/register');
+		} else {
+			var user = new User({
+				email: email,
+				password: password
+			});
+
+			// Check whether the email exists
+			user.checkUserExist(function (err, isExist) {
+				if (isExist) {
+					req.session.err = 'This email has been used.';
+					res.redirect('/user/register');
+				} else {
+					user.save(function (err, newUser) {
+						if (err) throw err;
+						req.session.succ = 'Register successfully';
+						res.redirect('/user/register');
+			        });
+				}
+			});
+		}
+	});
+
+	// Login
+	app.get('/user/login', function(req, res) {
+		res.render('user/login', {
+			title: 'Login'
+		});
+	});
+
+	app.post('/user/login', function(req, res) {
+		var email    = req.body.email;
+		var password = req.body.password;
+
+		if (isDataError(req, email, password)) {
+			res.redirect('/user/register');
+		} else {
+			var user = new User({
+				email: email,
+				password: password
+			});
+
+			user.auth(function (err, user) {
+				if (err) throw err;
+				if (user) {
+					req.session.isAuth = true;
+					req.session.user = user;
+					res.redirect('/');
+				} else {
+					req.session.err = 'Wrong email or password.';
+					res.redirect('/user/login');
+				}
+			});
+		}
+	});
+
+	// Logout
+	app.get('/user/logout', function(req, res) {
+		req.session.isAuth = false;
+		delete req.session.user;
+		req.session.succ = 'Logout successfully.';
+		res.redirect('/');
+	});
+
+	// Auth pages
+	app.get('/user/auth/info', function(req, res) {
+		res.render('user/auth/info', {
+			title: 'User Information',
+			user: req.session.user
+		});
+	});
+
+	
 };
